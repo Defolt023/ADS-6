8c490b25e267723fa709d6e10c6512e94ec6f9c9
DmitriyBarinov <95102030+Defolt023@users.noreply.github.com>
2022-04-18 21:52:52 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,31 +1,31 @@
 // Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TPQUEUE_H_
-#define INCLUDE_TPQUEUE_H_
+#ifndef INCLUDE_TPQUEUEH
+#define INCLUDE_TPQUEUEH
 
 template<typename T, int size>
 class TPQueue {
 private:
 T arr[100];
 int first, last;
-
-public:
-TPQueue() : first, last {}
+ 
+public: 
+TPQueue() : first(0), last(0) {}
 void push(T x) {
 int per = last++;
-while ((--per >= first) && (arr[per % size] < x)) {
+while ((--per >= first) && (arr[per % size].prior < x.prior)) { 
 arr[(per + 1) % size] = arr[per % size];
 }
 arr[(per + 1) % size] = x;
 }
 T pop() {
-return arr[(first) % size];
-}
+return arr[(first++) % size]; 
+} 
 }
 };
  
 struct SYM {
   char ch;
-  int prior;
+  int prior; 
 };
 
-#endif  // INCLUDE_TPQUEUE_H_
+#endif  // INCLUDE_TPQUEUEH
