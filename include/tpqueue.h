ca41ba5c2121380cbfb6f41f764377bc61a5f11e
DmitriyBarinov <95102030+Defolt023@users.noreply.github.com>
2022-04-18 21:56:12 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -7,25 +7,25 @@ class TPQueue {
 private:
 T arr[100];
 int first, last;
- 
-public: 
+
+public:
 TPQueue() : first(0), last(0) {}
 void push(T x) {
 int per = last++;
-while ((--per >= first) && (arr[per % size].prior < x.prior)) { 
+while ((--per >= first) && (arr[per % size].prior < x.prior)) {
 arr[(per + 1) % size] = arr[per % size];
 }
 arr[(per + 1) % size] = x;
 }
 T pop() {
-return arr[(first++) % size]; 
-} 
+return arr[(first++) % size];
+}
 }
 };
- 
+
 struct SYM {
   char ch;
-  int prior; 
+  int prior;
 };
 
 #endif  // INCLUDE_TPQUEUEH
